25d73bd164141b36bca0cb99d7652347def15c37
Rob Covington <owroc@yahoo.com>
2022-04-15 19:59:50 +0000

    updated script

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+now=$(date +"%m-%d-%Y-%H%M%S")
+
+#check if there is an old version of the hdhomerun application, if so archive it
+FILE=/home/owroc/docker/docker-hdhomerundvr/hdhomerun_record
+if test -f "$FILE"
+then
+	echo "Archiving old hdhomrun file"
+	mv hdhomerun_record /home/owroc/docker/docker-hdhomerundvr/archive/hdhomerun_record-$now
+else
+	echo"$FILE does not exist."
+fi
+
 #download the latest version of hdhomerun
-wget https://download.silicondust.com/hdhomerun/hdhomerun_record_linux
+wget -O hdhomerun_record https://download.silicondust.com/hdhomerun/hdhomerun_record_linux
 
 

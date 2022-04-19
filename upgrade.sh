a64b9de2ca949ffd01798ff3ed69cdb9e4cd2e76
Rob Covington <owroc@yahoo.com>
2022-04-19 02:04:35 +0000

    updates

--- upgrade.sh
+++ upgrade.sh
@@ -2,11 +2,13 @@
 
 now=$(date +"%m-%d-%Y-%H%M%S")
 
+cd /home/owroc/docker/docker-hdhomerundvr
+
 #check if there is an old version of the hdhomerun application, if so archive it
 FILE=/home/owroc/docker/docker-hdhomerundvr/hdhomerun_record
 if test -f "$FILE"
 then
-	echo "Archiving old hdhomrun file"
+	echo "Archiving old hdhomerun file"
 	mv hdhomerun_record /home/owroc/docker/docker-hdhomerundvr/archive/hdhomerun_record-$now
 else
 	echo"$FILE does not exist."
@@ -15,4 +17,14 @@ fi
 #download the latest version of hdhomerun
 wget -O hdhomerun_record https://download.silicondust.com/hdhomerun/hdhomerun_record_linux
 
+echo "Stopping hdhomerun container"
+docker stop hdhomerun
+echo "Deleting existing hdhomerun container"
+docker rm hdhomerun
+echo "Deleting existing hdhomerun image"
+docker rmi owroc/hdhomerun
+echo "Building new hdhomerun image"
+docker build -t owroc/hdhomerun .
+echo "Starting hdhomerun container"
+docker run -d --restart always --name hdhomerun --net=app_net --ip=192.168.1.204 -v /dvr:/hdhomerun owroc/hdhomerun
 
